9a1552bca7c01a18a292e6e7253980ac2a28df58
bidoudan <quran.fr09@gmail.com>
2020-01-14 10:44:59 +0100

    first commit

--- src/main/java/ma/ntdata/upload/file/controller/FileController.java
+++ src/main/java/ma/ntdata/upload/file/controller/FileController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
